2c1745d100cb18ba208da19bc9d436c7b17d5b79
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-06-01 09:14:12 -0700

    trackClick event conditionally sent to R

--- inst/htmlwidgets/igvShiny.js
+++ inst/htmlwidgets/igvShiny.js
@@ -14,6 +14,7 @@ HTMLWidgets.widget({
           options = {//locus: "MEF2C",
                minimumBases: 5,
                flanking: 1000,
+               doubleClickDelay: 1,
                showRuler: true,
                reference: {id: "hg38",
                      fastaURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg38/hg38.fa",
@@ -31,6 +32,34 @@ HTMLWidgets.widget({
                  ]
               }; // options
            igvBrowser = igv.createBrowser(igvDiv, options);
+           igvBrowser.on('trackclick', function (track, popoverData){
+              var x = popoverData;
+              if(x.length == 1){
+                  if(Object.getOwnPropertyNames(x[0]).includes("value")){
+                      var id = x[0].value;
+                      console.log("in click handler, id:" + id);
+                      if(id.indexOf("rs") == 0){
+                         //var url = "https://www.ncbi.nlm.nih.gov/snp/" + rsid;
+                         var url = "https://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=" + id
+                         return " &nbsp; dbSNP: <a href='" + url + "' target=_blank>" + id + "</a>";
+                         } // if "^rs"
+                      if(id.indexOf("tfbs-snp") == 0){
+                         console.log("--- about to contact Shiny")
+                         var message = {id: id, date: Date()};
+                         var messageName = "trackClick"
+                          Shiny.onInputChange(messageName, message);
+                         //Shiny.onInputChange("jsValue", {"value": Date() + id});
+                         console.log("--- after contacting Shiny")
+                         return "<h4> " + id + "</h4>";
+                         }
+                     } // if a value field
+                 } // if just one element
+              console.log("click! 810");
+              console.log(x);
+              //return undefined;   // true, false: default popup disabled; undefined: default popup ensues.
+               return undefined;
+              });
+
           },
       resize: function(width, height) {
         // TODO: code to re-render the widget with a new size
