49c6f5f1bb8904fa2fee18ee8cc391cf22646ebb
Karolina Scigocka <scigockakarolina@gmail.com>
2024-01-12 14:35:05 +0100

    added helper function for shinyapp tests

--- inst/unitTests/test_shinyApp.R
+++ inst/unitTests/test_shinyApp.R
@@ -1,6 +1,26 @@
 library(igvShiny)
 library(RUnit)
 library(shinytest2)
+
+#' @title test_click_and_check
+#' @description a helper function for clicking the button and checking if
+#' second parameter exist in html code
+#' 
+#' @param button_id string of button which will be clicked in the test
+#' @param expected_html_label string which attendance will be expected in html
+#' @param app AppDriver shinyApp using for test
+#'
+#' @rdname test_click_and_check
+#'
+#' @export
+#'
+test_click_and_check <- function(button_id, expected_html_label, app) {
+  app$click(button_id)
+  igv_html <- app$get_html(selector = "#igvShiny_0")
+  Sys.sleep(10)
+  expect_true(grepl(expected_html_label, igv_html))
+  print("good")
+}
 #----------------------------------------------------------------------------------------------------
 runAppTests <- function()
 {
@@ -10,30 +30,25 @@ runAppTests <- function()
 test_shinyAppDemo <- function()
 {
   message(sprintf("--- test_shinyAppDemo"))
+  options(chromote.timeout = 60)  
+  
   
   test_that("{shinytest2} recording: test_app", {
-  
+    
     sf <- system.file(package = "igvShiny", "demos", "igvShinyDemo.R")
-    app <- shinytest2::    AppDriver$new(
+    app <- AppDriver$new(
       app_dir = shiny::shinyAppFile(sf),
       name = "test_app",
       height = 695,
       width = 1235,
-      load_timeout = 1e+5,
-      timeout = 1e+5
+      load_timeout = 1e+6,
+      timeout = 1e+6
     )
-    app$set_inputs(igvReady = "igvShiny_0", allow_no_input_binding_ = TRUE, priority_ = "event")
-    app$set_inputs(igvReady = "igvShiny_0", allow_no_input_binding_ = TRUE, priority_ = "event")
-    Sys.sleep(15)
-    app$expect_values()
-    app$click("addBedGraphTrackButton")
-    app$set_inputs(currentGenomicRegion.igvShiny_0 = "chr1:7426230-7453241", allow_no_input_binding_ = TRUE, 
-                   priority_ = "event")
-    app$expect_values()
-    app$click("addGwasTrackButton")
-    app$set_inputs(currentGenomicRegion.igvShiny_0 = "chr19:45248107-45564645", allow_no_input_binding_ = TRUE, 
-                   priority_ = "event")
-    app$expect_values()
+    Sys.sleep(20)
+    test_click_and_check("addBedGraphTrackButton", "title=\"wig/bedGraph/local\"", app)
+    test_click_and_check("addBedGraphTrackFromURLButton", "title=\"bedGraph/remote\"", app)
+    test_click_and_check("addBamViaHttpButton", "title=\"1kg.bam\"", app)
+    test_click_and_check("addCramViaHttpButton", "title=\"CRAM\"", app)
   })
   
 } # test_shinyAppDemo
