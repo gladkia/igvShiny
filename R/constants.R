e338e637449f59ff21cc290d72503dc6b495ecce
Karolina Scigocka <scigockakarolina@gmail.com>
2024-02-07 14:14:06 +0100

    reformatted

--- R/constants.R
+++ R/constants.R
@@ -1,18 +1,16 @@
-CAS_GENOMES <-c(
-    "hg38",
-    "hg19",
-    "mm10",
-    "tair10",
-    "custom",
-    "dm6",
-    "sacCer3")
+CAS_GENOMES <-c("hg38",
+                "hg19",
+                "mm10",
+                "tair10",
+                "custom",
+                "dm6",
+                "sacCer3")
 
-BASIC_GENOMES <- c(
-    "hg38",
-    "hg19",
-    "mm10",
-    "tair10",
-    "rhos",
-    "custom",
-    "dm6",
-    "sacCer3")
+BASIC_GENOMES <- c("hg38",
+                   "hg19",
+                   "mm10",
+                   "tair10",
+                   "rhos",
+                   "custom",
+                   "dm6",
+                   "sacCer3")
